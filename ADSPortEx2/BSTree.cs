46854a098c844d1c1ac7546f1178dd5258883cf4
Hadookhan <hadikhan05@hotmail.co.uk>
2025-10-29 17:19:26 +0000

    Added Tests in Program. Need to fix Insertion in BST. Fixed Node Data method. Implemented EarliestGame + Height methods using my own Max helper function

--- ADSPortEx2/BSTree.cs
+++ ADSPortEx2/BSTree.cs
@@ -14,26 +14,60 @@ namespace ADSPortEx2
 
     class BSTree<T> : BinTree<T> where T : IComparable
     {
-
         public BSTree()
         {
-            throw new NotImplementedException();
+            root = null;
+        }
+
+        public BSTree(T item)
+        {
+            root = new Node<T>(item);
+        }
+
+        public BSTree(Node<T> node)
+        {
+            root = node;
         }
 
         //Functions for EX.2A
         public void InsertItem(T item)
         {
-            throw new NotImplementedException();
+            root = InsertHelper(item, root);
+        }
+
+        private Node<T> InsertHelper(T item, Node<T> tree)
+        {
+            if (tree == null)
+            {
+                return new Node<T>(item);
+            }
+
+            int val = item.CompareTo(tree.Data);
+
+            if (val < 0)
+            {
+                InsertHelper(item, tree.Left);
+            }
+            else if (val > 0)
+            {
+                InsertHelper(item, tree.Right);
+            }
+            else
+            {
+                throw new Exception("Cant have duplicate values");
+            }
+
+            return tree;
         }
 
         public int Height()
         {
-            throw new NotImplementedException();
+            return FindLongestPath(root);
         }
 
-        public T EarlieseGame()
+        public T EarliestGame()
         {
-            throw new NotImplementedException();
+            return FindEarliestGame(root);
         }
 
         //Functions for EX.2B
@@ -46,6 +80,38 @@ namespace ADSPortEx2
         public void Update(T item)
         {
             throw new NotImplementedException();
+        }
+
+        // HELPER FUNCTIONS
+        private int FindLongestPath(Node<T> root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            return 1 + Max(FindLongestPath(root.Left), FindLongestPath(root.Right));
+        }
+
+        private T FindEarliestGame(Node<T> root)
+        {
+            if (root.Left == null || root.Right == null)
+            {
+                return root.Data;
+            }
+
+            // Furthest Left Node is always earliest game
+            return FindEarliestGame(root.Left);
+        }
+
+        // I was thinking of using Math.Max, but decided to use my own implementation.
+        private int Max(int a, int b)
+        {
+            if (a >= b)
+            {
+                return a;
+            }
+            return b;
         }
 
         //Free space, use as necessary to address task requirements... 
--- ADSPortEx2/Program.cs
+++ ADSPortEx2/Program.cs
@@ -20,6 +20,21 @@ namespace ADSPortEx2
             // "2. Display all items... ect
             //}
 
+            var game1 = new VideoGame("Minecraft", "Notch", 2009);
+            var game2 = new VideoGame("Rainbow 6 Siege", "Ubisoft", 2015);
+            var game3 = new VideoGame("Black Ops 2", "Infinity Ward", 2012);
+            var game4 = new VideoGame("Tetris", "Dev1", 1995);
+
+            var Tree = new BSTree<VideoGame>(game1);
+            Tree.InsertItem(game2);
+            Tree.InsertItem(game4);
+            Tree.InsertItem(game3);
+
+            string buffer = "";
+            Tree.InOrder(ref buffer);
+            Console.WriteLine(buffer);
+            Console.WriteLine(Tree.EarliestGame().Title);
+            Console.WriteLine(Tree.Height());
 
 
 
